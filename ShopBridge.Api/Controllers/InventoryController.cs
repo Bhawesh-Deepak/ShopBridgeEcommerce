35eaa6703cf9d528a081e409e4a7bb337ef5b5a4
Bhawesh Deepak <Bhawesh Deepak@DESKTOP-KB2SSTK>
2023-01-28 03:42:38 -0800

    Exception handling in Api

--- ShopBridge.Api/Controllers/InventoryController.cs
+++ ShopBridge.Api/Controllers/InventoryController.cs
@@ -1,11 +1,14 @@
 ﻿
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using ShopBridge.Api.Helpers;
 using ShopBridge.Api.ModelDTO;
 using ShopBridge.Core.Entities.Products;
 using ShopBridge.Infrastructure.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ShopBridge.Api.Controllers
@@ -30,19 +33,53 @@ namespace ShopBridge.Api.Controllers
         [Consumes("application/json")]
         public async Task<IActionResult> CreateProduct(ProductModelDto model)
         {
-            var dbDto = _IMapper.Map<ProductModelDto, ProductModel>(model);
-            var response = await _IProductReposiyory.CreateEntity(dbDto);
-            return Ok("sdfshd");
+            try
+            {
+                var dbDto = _IMapper.Map<ProductModelDto, ProductModel>(model);
+                var response = await _IProductReposiyory.CreateEntity(dbDto);
+
+                if (response)
+                {
+                    return Ok(new ResponseHelper<ProductModelDto>(null, null, "Product has been created successfully!",
+                        HttpStatusCode.Created));
+                }
+                return BadRequest(new ResponseHelper<ProductModelDto>(null, null, "Something wents wrong, Please contact admin !",
+                        HttpStatusCode.BadRequest));
+
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Error(ex.Message, $"exception has been occured in " +
+                    $"{nameof(InventoryController)} in action {nameof(CreateProduct)}" +
+                    $" exception : {ex.InnerException}");
+
+                return BadRequest(new ResponseHelper<ProductModelDto>(null, null, "Something wents wrong, Please contact admin !",
+                       HttpStatusCode.BadRequest));
+            }
         }
+
 
         [HttpGet]
         [Produces("application/json")]
         [Consumes("application/json")]
         public async Task<IActionResult> GetProductList()
         {
-            var dbResponse = await _IProductReposiyory.GetAllEntities(x => x.IsDeleted == false);
-            var response = _IMapper.Map<List<ProductModel>, List<ProductModelDto>>(dbResponse.ToList());
-            return Ok(response);
+            try
+            {
+                var dbResponse = await _IProductReposiyory.GetAllEntities(x => x.IsDeleted == false);
+                var response = _IMapper.Map<List<ProductModel>, List<ProductModelDto>>(dbResponse.ToList());
+                return Ok(new ResponseHelper<ProductModelDto>(null, response, "Success", HttpStatusCode.OK));
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Error(ex.Message, $"exception has been occured in " +
+                     $"{nameof(InventoryController)} in action {nameof(CreateProduct)}" +
+                     $" exception : {ex.InnerException}");
+
+                return BadRequest(new ResponseHelper<ProductModelDto>(null, null, "Something wents wrong, Please contact admin !",
+                       HttpStatusCode.BadRequest));
+            }
+
         }
 
 
@@ -51,9 +88,28 @@ namespace ShopBridge.Api.Controllers
         [Consumes("application/json")]
         public async Task<IActionResult> UpdateProductDetail(ProductModelDto model)
         {
-            var updateModel = _IMapper.Map<ProductModelDto, ProductModel>(model);
-            var response = await _IProductReposiyory.UpdateEntity(updateModel);
-            return Ok("");
+            try
+            {
+                var updateModel = _IMapper.Map<ProductModelDto, ProductModel>(model);
+                var response = await _IProductReposiyory.UpdateEntity(updateModel);
+                if (response)
+                {
+                    return Ok(new ResponseHelper<ProductModelDto>(null, null, "Product has been updated successfully!",
+                        HttpStatusCode.Created));
+                }
+                return BadRequest(new ResponseHelper<ProductModelDto>(null, null, "Something wents wrong, Please contact admin !",
+                        HttpStatusCode.BadRequest));
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Error(ex.Message, $"exception has been occured in " +
+                         $"{nameof(InventoryController)} in action {nameof(UpdateProductDetail)}" +
+                         $" exception : {ex.InnerException}");
+
+                return BadRequest(new ResponseHelper<ProductModelDto>(null, null, "Something wents wrong, Please contact admin !",
+                       HttpStatusCode.BadRequest));
+            }
+
         }
 
         [HttpDelete]
@@ -62,13 +118,33 @@ namespace ShopBridge.Api.Controllers
 
         public async Task<IActionResult> DeleteProduct(int productId)
         {
-            var deleteModel = await _IProductReposiyory.GetSingleEntity(x => x.Id == productId);
-            if (deleteModel != null)
+            try
             {
-                deleteModel.IsDeleted = true;
+                var deleteModel = await _IProductReposiyory.GetSingleEntity(x => x.Id == productId);
+                if (deleteModel != null)
+                {
+                    deleteModel.IsDeleted = true;
+                }
+                var deleteResponse = await _IProductReposiyory.DeleteEntity(deleteModel);
+                if (deleteResponse)
+                {
+                    return Ok(new ResponseHelper<ProductModelDto>(null, null, "Product has been updated successfully!",
+                        HttpStatusCode.Created));
+                }
+                return BadRequest(new ResponseHelper<ProductModelDto>(null, null, "Something wents wrong, Please contact admin !",
+                        HttpStatusCode.BadRequest));
             }
-            var deleteResponse = await _IProductReposiyory.DeleteEntity(deleteModel);
-            return Ok("");
+            catch (Exception ex)
+            {
+                Serilog.Log.Error(ex.Message, $"exception has been occured in " +
+                        $"{nameof(InventoryController)} in action {nameof(DeleteProduct)}" +
+                        $" exception : {ex.InnerException}");
+
+                return BadRequest(new ResponseHelper<ProductModelDto>(null, null, "Something wents wrong, Please contact admin !",
+                       HttpStatusCode.BadRequest));
+
+            }
+
         }
     }
 }
